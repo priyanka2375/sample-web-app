e67df496c556ce6c30f6201cca45998b38bc13de
Sneha Kanekar <skanekar@us.ibm.com>
2020-12-14 11:10:10 +0000

    Update create-multi-arch.sh

--- create-multi-arch.sh
+++ create-multi-arch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#export DOCKER_CLI_EXPERIMENTAL=enabled
+export DOCKER_CLI_EXPERIMENTAL=enabled
 
-podman manifest create quay.io/snehakpersistent/multi-arch-travis:latest \
+docker manifest create quay.io/snehakpersistent/multi-arch-travis:latest \
 quay.io/snehakpersistent/multi-arch-travis:x86_64 quay.io/snehakpersistent/multi-arch-travis:ppc64le
 
-podman manifest inspect quay.io/snehakpersistent/multi-arch-travis
+docker manifest inspect quay.io/snehakpersistent/multi-arch-travis
 
-podman login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
+docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
 
-podman manifest push quay.io/snehakpersistent/multi-arch-travis:latest
+docker manifest push quay.io/snehakpersistent/multi-arch-travis:latest
