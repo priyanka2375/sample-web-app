6f77239c53e55ca8088811c2f842bd14572ca105
Sneha Kanekar <skanekar@us.ibm.com>
2020-12-14 09:15:58 +0000

    Switch to podman

--- create-multi-arch.sh
+++ create-multi-arch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export DOCKER_CLI_EXPERIMENTAL=enabled
+#export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker manifest create quay.io/snehakpersistent/multi-arch-travis:latest \
+podman manifest create quay.io/snehakpersistent/multi-arch-travis:latest \
 quay.io/snehakpersistent/multi-arch-travis:x86_64 quay.io/snehakpersistent/multi-arch-travis:ppc64le
 
-docker manifest inspect quay.io/snehakpersistent/multi-arch-travis
+podman manifest inspect quay.io/snehakpersistent/multi-arch-travis
 
-docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
+podman login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
 
-docker manifest push quay.io/snehakpersistent/multi-arch-travis:latest
+podman manifest push quay.io/snehakpersistent/multi-arch-travis:latest
