2e244793f5264854b2f25c3a62e2f9596757f560
Sneha Kanekar <skanekar@us.ibm.com>
2020-12-14 11:08:47 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 ARCH=$(uname -m)
 
-podman build -f ./$ARCH/Dockerfile -t quay.io/snehakpersistent/multi-arch-travis:$ARCH .
-podman login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
-podman push quay.io/snehakpersistent/multi-arch-travis:$ARCH
+docker build -f ./$ARCH/Dockerfile -t quay.io/snehakpersistent/multi-arch-travis:$ARCH .
+docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
+docker push quay.io/snehakpersistent/multi-arch-travis:$ARCH
