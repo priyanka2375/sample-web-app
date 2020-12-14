0d606bd4d5a03304c1c94681e27d63079a8af91f
Sneha Kanekar <skanekar@us.ibm.com>
2020-12-14 11:09:36 +0000

    Update build_app.sh

--- build_app.sh
+++ build_app.sh
@@ -2,6 +2,6 @@
 
 ARCH=$(uname -m)
 
-podman build -f ./app/Dockerfile -t quay.io/snehakpersistent/sample-php-app:$ARCH .
-podman login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
-podman push quay.io/snehakpersistent/sample-php-app:$ARCH
+docker build -f ./app/Dockerfile -t quay.io/snehakpersistent/sample-php-app:$ARCH .
+docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
+docker push quay.io/snehakpersistent/sample-php-app:$ARCH
