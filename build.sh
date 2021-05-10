83d1b7a3e4d45d1bd3c9bb06454c96ed014b7af4
snehakpersistent <Sneha.Kanekar1@ibm.com>
2021-05-10 12:55:12 +0000

    Code cleanup

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ARCH=$(uname -m)
-
-sudo podman build --no-cache -f ./$ARCH/Dockerfile -t quay.io/snehakpersistent/multi-arch-image:$ARCH . --format docker
-sudo podman login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
-sudo podman push quay.io/snehakpersistent/multi-arch-image:$ARCH
--- build_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ARCH=$(uname -m)
-
-sudo podman pull quay.io/snehakpersistent/multi-arch-image:latest
-sudo podman rmi $(sudo podman images -f "dangling=true" -q)
-sudo podman build -f ./app/Dockerfile -t quay.io/snehakpersistent/sample-php-app:$ARCH .
-sudo podman login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
-sudo podman push quay.io/snehakpersistent/sample-php-app:$ARCH
